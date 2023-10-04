9c004f534378fec57598c206efde9efe88df41f4
Sharon <124261315+codersharon@users.noreply.github.com>
2023-10-04 14:12:39 +0530

    Update navbar.tsx

--- pages/comps/navbar.tsx
+++ pages/comps/navbar.tsx
@@ -137,7 +137,7 @@ function ResponsiveAppBar() {
               textDecoration: "none",
             }}
           >
-            TechWave
+            TechVave
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
