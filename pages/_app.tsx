d56a0e3ba96aa9d02b4e823a5b61523092b52f0f
Sharon <sharonsandeep46655@gmail.com>
2023-10-08 14:11:12 +0530

    nav-ud-7

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,9 +25,11 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
+          href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
           rel="stylesheet"
-          href="https://fonts.googleapis.com/icon?family=Material+Icons"
         />
       </Head>
 
